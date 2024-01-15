2ef9f26adfb0fa97ff986e4ff42156c60b625c7c
vieta <topologicrose@protonmail.com>
2024-01-15 23:00:42 +0100

    add more tewst

--- tests/ini.py
+++ tests/ini.py
@@ -6,10 +6,10 @@ class TestIniFile(unittest.TestCase):
         folder_path = '../ini'
         ini_file_name = 'config.ini'
         ini_file_path = os.path.join(folder_path, ini_file_name)
-    def test_ini_file_exists(self):
-        folder_path = '../ini'
-        ini_file_name = 'mail.ini'
-        ini_file_path = os.path.join(folder_path, ini_file_name)
+    #def test_ini_file_exists(self):
+    #    folder_path = '../ini'
+    #    ini_file_name = 'mail.ini'
+    #    ini_file_path = os.path.join(folder_path, ini_file_name)
 
         
         self.assertTrue(os.path.isfile(ini_file_path), f"INI file '{ini_file_name}' does not exist in folder '{folder_path}'")
